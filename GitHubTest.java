287bef5b5910913058c181069d374bde49331d71
wengithub1 <49420052+wengithub1@users.noreply.github.com>
2019-04-09 09:09:58 +0800

    second commit

--- GitHubTest.java
+++ GitHubTest.java
@@ -2,6 +2,6 @@ package day07;
 
 public class GitHubTest {
     public static void main(String[] args) {
-        System.out.println("First GitHub Project");
+        System.out.println("Second GitHub Project");
     }
 }
